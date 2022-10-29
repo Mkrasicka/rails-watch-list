2c0aacd12f49ecce4c404d97555c9a00cd9c5bf7
magdalena krasicka <krasickamagdalena@gmail.com>
2022-10-29 09:20:20 +0100

    done

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,6 +1,7 @@
 class List < ApplicationRecord
   has_many :bookmarks, dependent: :destroy
   has_many :movies, through: :bookmarks, dependent: :destroy
+  has_many_attached :photo
   # you can;t destro if theere is a bookmark
   validates :name, uniqueness: true, presence: true
 end
